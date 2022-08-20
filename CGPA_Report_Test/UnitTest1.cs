d195c478d40c79204f133f1ce8e1e1a2b2b02c6a
Areola John <areolajohnbayo@outlook.com>
2022-08-20 23:13:53 +0100

    Updated

--- CGPA_Report_Test/UnitTest1.cs
+++ CGPA_Report_Test/UnitTest1.cs
@@ -11,16 +11,17 @@ namespace CGPA_Report_Test
         [DataRow("5","5","5","5","5","5")]
         [DataRow("4", "4", "4", "4", "4", "4")]
         [DataRow("3", "3", "3", "3", "3", "3")]
-        public void TestMethod1()
+        public void TestMethod1(string Mathematics, string English_Language, string Finance, string Economics, string Physics, string expected)
         {
-            int Mathematics = int.Parse(Mathematics);
-            int English_Language =int.Parse(English_Language);
+            int MathematicsScore = int.Parse(Mathematics);
+            int English_LanguageScore =int.Parse(English_Language);
             int FinanceScore = int.Parse(Finance);
             int EconomicsScore = int.Parse(Economics);
-            int PhysicsScore= int. Pa
+            int PhysicsScore = int.Parse(Physics);
             var Student_CGPA = new Student_Result();
-            var TotalCgpa = Student_CGPA.TotalCgpa(5, 5, 5, 5, 5);
-            Assert.AreEqual(5, TotalCgpa);
+            int expectedResult = int.Parse(expected);
+            var TotalCgpa = Student_CGPA.TotalCgpa(MathematicsScore, English_LanguageScore,FinanceScore, EconomicsScore, PhysicsScore);
+            Assert.AreEqual(expectedResult, TotalCgpa);
 
 
         }
